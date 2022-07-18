65362ad929524f0ce3a766f0b80baed020b8a71a
NataliGoIt <91018314+NataliGoIt@users.noreply.github.com>
2022-07-18 14:34:47 +0000

    Deploying to gh-pages from @ NataliGoIt/new@0b5c3829dc3a2877d830a289593cf7255c29b980 🚀

--- src.8784fd7e.js
+++ src.8784fd7e.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/new/src.356a0a56.js.map
+//# sourceMappingURL=/new/src.8784fd7e.js.map
